c9067c0c538d4a579a844b9c5ea2ab9597c472d6
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-26 10:51:17 +0200

    Adding passing exercises part03 18-22

---
+++ part03/PrintInStars.java
@@ -0,0 +1,24 @@
+
+public class Printer {
+
+    public static void main(String[] args) {
+        // You can test the method here
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
+    }
+    
+    public static void printStars(int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+    
+    public static void printArrayInStars(int[] array) {
+        // Write some code in here
+        for (int i = 0; i < array.length; i++) {
+            printStars(array[i]);
+        }
+    }
+
+}
