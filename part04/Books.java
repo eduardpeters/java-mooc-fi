1e9050c204688d1e37ea738c916a8522b8cc5692
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-28 20:38:07 +0200

    Adding passing exercises part04 17-20

---
+++ part04/Books.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // implement here the program that allows the user to enter 
+        // book information and to examine them
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList<>();
+
+        while (true) {
+            System.out.print("Title: ");
+            String name = scanner.nextLine();
+
+            if (name.isEmpty()) {
+                break;
+            }
+
+            System.out.print("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+
+            System.out.print("Publication year: ");
+            int publicationYear = Integer.valueOf(scanner.nextLine());
+
+            books.add(new Book(name, pages, publicationYear));
+        }
+
+        System.out.println("What information will be printed? ");
+        String choice = scanner.nextLine();
+
+        if (choice.equals("everything")) {
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        } else if (choice.equals("name")) {
+            for (Book book : books) {
+                System.out.println(book.getName());
+            }
+        }
+    }
+}
