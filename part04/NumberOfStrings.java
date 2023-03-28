9b6aee2362c8f83dd8ff537bda37f04b5c67a302
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-28 21:14:40 +0200

    Adding passing exercises part04 21-25

---
+++ part04/NumberOfStrings.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class NumberOfStrings {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int stringCount = 0;
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("end")) {
+                break;
+            }
+
+            stringCount += 1;
+        }
+
+        System.out.println(stringCount);
+    }
+}
