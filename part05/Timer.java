f1776887397b1a91c5a3dff24ec689af747ba209
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-03-31 08:30:06 +0200

    Adding passing exercises part05 01-04

---
+++ part05/Timer.java
@@ -0,0 +1,24 @@
+
+public class Timer {
+
+    private ClockHand seconds;
+    private ClockHand hundreths;
+
+    public Timer() {
+        this.seconds = new ClockHand(60);
+        this.hundreths = new ClockHand(100);
+    }
+
+    public void advance() {
+        this.hundreths.advance();
+
+        if (this.hundreths.value() == 0) {
+            this.seconds.advance();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return seconds + ":" + hundreths;
+    }
+}
